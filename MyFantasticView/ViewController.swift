57c9a9c964f15258daf5cec22a6b60c1aa3ae317
venco4 <venco4@sg.uob>
2019-08-06 12:05:22 +0800

    Initial Commit

---
+++ MyFantasticView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFantasticView
+//
+//  Created by venco4 on 6/8/19.
+//  Copyright © 2019 venco4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
