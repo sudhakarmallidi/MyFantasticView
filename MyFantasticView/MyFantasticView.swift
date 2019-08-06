cb26079e4f0ed1c3cfad202a475c4dc247dfd08d
venco4 <venco4@sg.uob>
2019-08-06 12:16:36 +0800

    first commit

---
+++ MyFantasticView/MyFantasticView.swift
@@ -0,0 +1,21 @@
+//
+//  MyFantasticView.swift
+//  MyFantasticView
+//
+//  Created by venco4 on 6/8/19.
+//  Copyright © 2019 venco4. All rights reserved.
+//
+
+import UIKit
+
+class MyFantasticView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
